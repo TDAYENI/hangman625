51f1fe5b7351c7e26e8506ddc5d6821cbf18b4d9
TDAYENI <titilayoay@googlemail.com>
2024-05-28 16:50:37 +0100

    created working check_guess func

--- milestone_3.py
+++ milestone_3.py
@@ -1,14 +1,25 @@
 from  milestone_2 import word
 print(word)
-#guess =input('Enter a single letter: ')
+
+def check_guess (guess):
+    while True:
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+guess =input('Enter a single letter: ')
+check_guess(guess)
+
+""" #guess =input('Enter a single letter: ')
 while True:
     guess =input('Enter a single letter: ')
     if len(guess) == 1 and guess.isalpha():
         break
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print("Invalid letter. Please, enter a single alphabetical character.") """
 
-if guess in word.lower():
+""" if guess in word.lower():
     print(f'"Good guess! {guess} is in the word."')
 else:
-    print('failed')
+    print(f'"Sorry, {guess} is not in the word. Try again."') """
