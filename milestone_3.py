b728e34ca7c2f89d32c54c490cacaecb3a41eedd
TDAYENI <titilayoay@googlemail.com>
2024-05-28 16:57:55 +0100

    m3 task 3 comp func check_guess & ask_for_input

--- milestone_3.py
+++ milestone_3.py
@@ -7,19 +7,15 @@ def check_guess (guess):
             break
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
+    ask_for_input(guess)
+
+
+def ask_for_input(guess):
+    if guess.lower() in word.lower():
+        print(f'"Good guess! {guess} is in the word."')
+    else:
+        print(f'"Sorry, {guess} is not in the word. Try again."')
+
 
 guess =input('Enter a single letter: ')
 check_guess(guess)
-
-""" #guess =input('Enter a single letter: ')
-while True:
-    guess =input('Enter a single letter: ')
-    if len(guess) == 1 and guess.isalpha():
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.") """
-
-""" if guess in word.lower():
-    print(f'"Good guess! {guess} is in the word."')
-else:
-    print(f'"Sorry, {guess} is not in the word. Try again."') """
