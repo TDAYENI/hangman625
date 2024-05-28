16c38b24d61c32f332b3ab1fe72623618873dcaf
TDAYENI <titilayoay@googlemail.com>
2024-05-28 15:40:37 +0100

    milestone_3 Task1

---
+++ milestone_3.py
@@ -0,0 +1,7 @@
+#guess =input('Enter a single letter: ')
+while True:
+    guess =input('Enter a single letter: ')
+    if len(guess) == 1 and guess.isalpha():
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
