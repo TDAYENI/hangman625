9490e5f3ad373a05539532490d19ed021171f9ff
TDAYENI <titilayoay@googlemail.com>
2024-05-28 14:54:39 +0100

    Task 4 comp

--- milestone_2.py
+++ milestone_2.py
@@ -4,4 +4,7 @@ word = random.choice(word_list)
 print(word)
 
 guess =input('Enter a single letter: ')
-print(guess)
+if len(guess) == 1 and guess.isalpha():
+    print('"Good guess!"')
+else:
+    print("Oops! That is not a valid input.")
