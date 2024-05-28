707be51aa351035ea3c5d0e87638176b2361126e
TDAYENI <titilayoay@googlemail.com>
2024-05-28 17:40:42 +0100

    unfinished Hangman Class

--- milestone_2.py
+++ milestone_2.py
@@ -9,8 +9,3 @@ word_list = ["Durian", "Orange", "Apple", "Mango", "Pineapple"]
 word = choose_random_word(word_list)
 
 
-""" guess =input('Enter a single letter: ')
-if len(guess) == 1 and guess.isalpha():
-    print('"Good guess!"')
-else:
-    print("Oops! That is not a valid input.") """
