296cc3e6166ca8cdf11f307c8863a57c6aa58f23
TDAYENI <titilayoay@googlemail.com>
2024-05-30 14:20:17 +0100

    m2.py unqiue word code tested and works

--- milestone_2.py
+++ milestone_2.py
@@ -7,3 +7,8 @@ word_guessed = ['_' for i in range(len(word))]
 
 print(word_guessed)
 print(len(word_guessed))
+
+num_letters = len(set(word))
+print(f'unique word count {num_letters}')
+
+print(f'unique words {set(word)}')
