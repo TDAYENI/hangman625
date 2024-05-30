8edb726121a644c2fff43a87b4b6c0afa45e0e49
TDAYENI <titilayoay@googlemail.com>
2024-05-30 16:01:43 +0100

    for loop to append _ with guessed letter works when tested

--- milestone_2.py
+++ milestone_2.py
@@ -1,14 +1,23 @@
 import random
-
+# removing a random word from a list
 word_list = ["Durian", "Orange", "Apple", "Mango", "Pineapple"]
 word=random.choice(word_list)
 print(word)
+# list comp creates list of "_" equal to word len
 word_guessed = ['_' for i in range(len(word))]
-
+guess = 'a'
 print(word_guessed)
 print(len(word_guessed))
-
+#number of unique words
 num_letters = len(set(word))
 print(f'unique word count {num_letters}')
-
+#unique letters in word
 print(f'unique words {set(word)}')
+
+for i in range(len(word)):
+     if guess == word.lower()[i]:
+          word_guessed[i] = word[i]
+
+
+print(word_guessed)
+print(word)
