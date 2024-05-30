b81906492771a2bd33ae868600e9974650f4a2bd
TDAYENI <titilayoay@googlemail.com>
2024-05-30 19:25:45 +0100

    added words file and imported it into hangman.py

---
+++ words/list_of_words.py
@@ -0,0 +1,6 @@
+words_to_guess = [
+    "apple", "banana", "orange", "pear", "strawberry",
+    "watermelon", "pineapple", "grape", "blueberry", "kiwi",
+    "mango", "peach", "lemon", "lime", "coconut",
+    "apricot", "cherry", "fig", "papaya", "plum"
+]
