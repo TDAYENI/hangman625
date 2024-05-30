14d2c1949a32c51dbebc8a9a80f01608b31fc302
TDAYENI <titilayoay@googlemail.com>
2024-05-30 12:03:21 +0100

    modifief m3 py but will del from main

--- milestone_3.py
+++ milestone_3.py
@@ -19,3 +19,12 @@ def ask_for_input(guess):
 
 guess =input('Enter a single letter: ')
 check_guess(guess)
+
+    """
+word: The word to be guessed
+word_guessed:A list of the letters of the word, with _ for each letter not yet guessed.
+num_letters:he number of UNIQUE letters in the word that have not been guessed yet
+num_lives: int - The number of lives the player has at the start of the game.
+word_list: list - A list of words
+list_of_guesses: list - A list of the guesses that have already been tried. Set this to an empty list initially
+    """
